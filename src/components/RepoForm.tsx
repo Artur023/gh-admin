b1274aa9fec37ec2e516ae477ddfdac0af382e36
Artur023 <artur.023@mail.ru>
2025-03-21 16:10:31 +0400

    fix: fix

--- src/components/RepoForm.tsx
+++ src/components/RepoForm.tsx
@@ -81,7 +81,7 @@ const RepoForm: React.FC<RepoFormProps> = ({token, owner}) => {
                 className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 disabled:opacity-50"
             >
 
-                {createMutation.isLoading ? "Создание..." : "Создать"}
+                {createMutation.status ? "Создание..." : "Создать"}
             </button>
             {createMutation.isError && (
                 <p className="mt-2 text-red-500">
